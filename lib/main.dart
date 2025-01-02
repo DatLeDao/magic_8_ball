8d0c3c48590ed769164280cf0615c5ffc512b712
“DatLeDao” <106359186+DatLeDao@users.noreply.github.com>
2025-01-02 18:49:10 +0700

    app added

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(
+        backgroundColor: Colors.green[600],
+        title: Text('magic 8 ball'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  _BallState createState() {
+    return _BallState();
+  }
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            // Random.nextInt(n) returns a random integer from 0 to n-1
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        // Adding images
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
